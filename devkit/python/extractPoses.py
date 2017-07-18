d5ff9e759981964cc1090f156a97dff4e36f2fc6
racinmat <racinmat@fel.cvut.cz>
2017-07-18 23:03:17 +0200

    Done loading tracklets from xml.

--- devkit/python/extractPoses.py
+++ devkit/python/extractPoses.py
@@ -5,31 +5,30 @@ from smop.core import *
 def extractPoses(poses=None):
     # EXTRACTPOSES extracts poses from subtree of labels.xml
 
-    nPoses = str2double(poses.getElementsByTagName('count').item(0).getTextContent)
-    item_version = str2double(poses.getElementsByTagName('item_version').item(0).getTextContent)
-    posesVec = zeros(14, nPoses)
-    poseChildren = poses.getElementsByTagName('item')
-    for poseIdx in arange(0, poseChildren.getLength - 1).reshape(-1):
-        p = poseChildren.item(poseIdx)
-        posesVec[1, poseIdx + 1] = str2double(p.getElementsByTagName('tx').item(0).getTextContent)
-        posesVec[2, poseIdx + 1] = str2double(p.getElementsByTagName('ty').item(0).getTextContent)
-        posesVec[3, poseIdx + 1] = str2double(p.getElementsByTagName('tz').item(0).getTextContent)
-        posesVec[4, poseIdx + 1] = str2double(p.getElementsByTagName('rx').item(0).getTextContent)
-        posesVec[5, poseIdx + 1] = str2double(p.getElementsByTagName('ry').item(0).getTextContent)
-        posesVec[6, poseIdx + 1] = str2double(p.getElementsByTagName('rz').item(0).getTextContent)
-        posesVec[7, poseIdx + 1] = str2double(p.getElementsByTagName('state').item(0).getTextContent)
-        posesVec[8, poseIdx + 1] = str2double(p.getElementsByTagName('occlusion').item(0).getTextContent)
-        posesVec[9, poseIdx + 1] = str2double(p.getElementsByTagName('occlusion_kf').item(0).getTextContent)
-        posesVec[10, poseIdx + 1] = str2double(p.getElementsByTagName('truncation').item(0).getTextContent)
-        posesVec[11, poseIdx + 1] = str2double(p.getElementsByTagName('amt_occlusion').item(0).getTextContent)
-        posesVec[12, poseIdx + 1] = str2double(p.getElementsByTagName('amt_occlusion_kf').item(0).getTextContent)
-        posesVec[13, poseIdx + 1] = str2double(p.getElementsByTagName('amt_border_l').item(0).getTextContent)
-        posesVec[14, poseIdx + 1] = str2double(p.getElementsByTagName('amt_border_r').item(0).getTextContent)
-        posesVec[15, poseIdx + 1] = str2double(p.getElementsByTagName('amt_border_kf').item(0).getTextContent)
-
+    nPoses = int(poses.count.text)
+    item_version = int(poses.item_version.text)
+    posesVec = np.zeros((15, nPoses), dtype=float)
+    poseIdx = 0
+    for pose in poses.find_all('item', recursive=False):
+        posesVec[0, poseIdx] = float(pose.tx.text)
+        posesVec[1, poseIdx] = float(pose.ty.text)
+        posesVec[2, poseIdx] = float(pose.tz.text)
+        posesVec[3, poseIdx] = float(pose.rx.text)
+        posesVec[4, poseIdx] = float(pose.ry.text)
+        posesVec[5, poseIdx] = float(pose.rz.text)
+        posesVec[6, poseIdx] = float(pose.state.text)
+        posesVec[7, poseIdx] = float(pose.occlusion.text)
+        posesVec[8, poseIdx] = float(pose.occlusion_kf.text)
+        posesVec[9, poseIdx] = float(pose.truncation.text)
+        posesVec[10, poseIdx] = float(pose.amt_occlusion.text)
+        posesVec[11, poseIdx] = float(pose.amt_occlusion_kf.text)
+        posesVec[12, poseIdx] = float(pose.amt_border_l.text)
+        posesVec[13, poseIdx] = float(pose.amt_border_r.text)
+        posesVec[14, poseIdx] = float(pose.amt_border_kf.text)
+        poseIdx += 1
 
         ## plausibility check
-    if nPoses != poseIdx + 1:
-        fprintf(2, 'number of poses (%d) does not match count (%d)!', nPoses, poseIdx + 1)
+    if nPoses != poseIdx:
+        fprintf(2, 'number of poses (%d) does not match count (%d)!', nPoses, poseIdx)
 
     return posesVec
--- devkit/python/readTracklets.py
+++ devkit/python/readTracklets.py
@@ -17,38 +17,24 @@ def readTracklets(filename=None):
         trackletsElement = root.tracklets
 
     objIdx = 0
-    count = 0
+
+    count = int(trackletsElement.find('count', recursive=False).text)
     tracklets = []
 
-    for element in trackletsElement.children:
-        if element == "\n":
-            continue
-
-        # fprintf('No attributes\n')
-        # else
-        # fprintf('found: #s\n',char(element.getTagName))
-        if element.name == 'count':
-            count = int(element.text)
-            tracklets = []
-        # fprintf( '#d\n', count );
-        # print out version info
-        # elseif strcmp(element.getTagName,'item_version') # meta-data
-        # fprintf( '#d\n', str2double(element.getFirstChild.getData) );
-        elif element.name == 'item':
-                tracklet = {}
-                tracklet.objectType = str(element.objecttype.text)
-                tracklet.h = float(element.h.text)
-                tracklet.w = float(element.w.text)
-                tracklet.l = float(element.l.text)
-                tracklet.first_frame = int(element.first_frame.text)
-                poses = element.getElementsByTagName('poses').item(0)
-                tracklet.poses = extractPoses(poses)
-                tracklet.finished = bool(element.finished.text)
-                tracklets.append(tracklet)
-                objIdx += 1
+    for element in trackletsElement.find_all('item', recursive=False):
+        tracklet = {}
+        tracklet['objectType'] = str(element.objecttype.text)
+        tracklet['h'] = float(element.h.text)
+        tracklet['w'] = float(element.w.text)
+        tracklet['l'] = float(element.l.text)
+        tracklet['first_frame'] = int(element.first_frame.text)
+        tracklet['poses'] = extractPoses(element.poses)
+        tracklet['finished'] = bool(element.finished.text)
+        tracklets.append(tracklet)
+        objIdx += 1
 
     # plausibility check
-    if count != objIdx - 1:
+    if count != objIdx:
         fprintf(2, 'number of tracklets (%d) does not match count (%d)!', objIdx, count)
 
     return tracklets
