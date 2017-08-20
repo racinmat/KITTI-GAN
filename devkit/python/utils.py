Fixed angle calculation.

devkit/python/utils.py
@@ -6,6 +6,7 @@ from functools import lru_cache
 import diskcache
 import time
 
+
 def readVariable(data=None, name=None, M=None, N=None):
     if name not in data:
         return []
@@ -18,7 +19,7 @@ def readVariable(data=None, name=None, M=None, N=None):
         return data[name]
 
 
-# @lru_cache(maxsize=32)
+@lru_cache(maxsize=32)
 def loadFromFile(fname, columns, dtype):
     with open(fname, 'rb') as f:
         result = np.fromfile(f, dtype).reshape((-1, columns))
@@ -32,7 +33,7 @@ def size(a, b=0):
     # a is not a scalar
     try:
         if b:
-            return s[b-1]
+            return s[b - 1]
         else:
             return s
     except IndexError:
@@ -63,6 +64,25 @@ def timeit(method):
 
     return timed
 
+
+class Timeit(object):
+
+    def __init__(self, method) -> None:
+        self.method = method
+        self.time = 0
+
+    def __call__(self, *args, **kw):
+        a = time.time()
+        result = self.method(*args, **kw)
+        b = time.time()
+        milis = int(round((b - a) * 1000))
+
+        self.time = self.time + milis
+
+        return result
+
+    def get_time(self):
+        return "total time of {} is {} milis".format(self.method.__name__, self.time)
 
 def transform_to_range(from_min, from_max, to_min, to_max, value):
     from_int = from_max - from_min

extract-data.py
@@ -8,16 +8,17 @@ import glob
 from PIL import Image
 from devkit.python.readTracklets import read_tracklets
 import numpy as np
-from devkit.python.utils import load_image
+from devkit.python.utils import load_image, Timeit
 from math import pi
 import pickle
 import os
-from utils import tracklet_to_bounding_box, is_tracklet_seen, Cache, get_pointcloud, \
-    pointcloud_to_image, sample_to_image
+from utils import tracklet_to_bounding_box, is_tracklet_seen, Cache, get_pointcloud, pointcloud_to_image
+from devkit.python.utils import timeit
 
-cache_bb = Cache('./cache/bb')
 
-atexit.register(lambda: cache_bb.close())
+# cache_bb = Cache('./cache/bb')
+#
+# atexit.register(lambda: cache_bb.close())
 
 
 # @lru_cache(maxsize=32)
@@ -46,20 +47,24 @@ def is_for_dataset(tracklet, frame, calib_dir, cam):
     treshold = treshold_degrees * pi / 180
     # filter out cars with high rotation
 
-    corners, t, rz, box, corners_3D, pose_idx = tracklet_to_bounding_box(tracklet=tracklet,
-                                                                         cam=cam,
-                                                                         frame=frame,
-                                                                         calib_dir=calib_dir)
+    corners, t, rz, box, corners_3D, pose_idx, orientation_3D = tracklet_to_bounding_box(tracklet=tracklet,
+                                                                                         cam=cam,
+                                                                                         frame=frame,
+                                                                                         calib_dir=calib_dir)
 
     # Rotation is calculated in 3D coordinates.
-    # Bounding box is transferred to cylindrical coordinates (so we do not care about the Y axis). [x, z => r, theta, y => y]
+    # Orientation of bounding box is transferred to cylindrical coordinates (so we do not care about the Y axis). [x, z => r, theta, y => y]
     # Angle in cylindrical coordinates is ange under which car is seen, so it is used for filtering.
-    theta = np.arctan2(corners_3D[2, :], corners_3D[0, :]) - np.pi / 2  # so theta == 0 is angle of car in front of camera
-    r = np.linalg.norm((corners_3D[0, :], corners_3D[2, :]), axis=0)
-    if theta > treshold or theta < - treshold:
+    # orientation_3D is represented by 2 points.
+    # car is in front of camera when angle of both points (in cylindrical coordinates) is same
+    theta = np.arctan2(orientation_3D[2, :], orientation_3D[0, :])
+    r = np.linalg.norm((corners_3D[0, :], corners_3D[2, :]), axis=0)    # r is used for distance measurement
+    angle = theta[1] - theta[0]
+    if angle > treshold or angle < - treshold:
         return False
 
-    distance = r[7]  # instead of fixed distance in X axis, we use distance from cylindrical coordinates, because this is more accurate
+    distance = r[
+        7]  # instead of fixed distance in X axis, we use distance from cylindrical coordinates, because this is more accurate
     # corner_ldf = corners_3D[:, 7]
     # distance = corner_ldf.T[2]
     if distance < min_distance or distance > max_distance:
@@ -73,14 +78,14 @@ def velodyne_data_exist(base_dir, frame):
     return os.path.isfile(filename)
 
 
-# @timeit
+@Timeit
 def get_x_y_data_for(tracklet, frame, cam, calib_dir, current_dir, with_image=False, grayscale=False):
     image_resolution = np.array([1242, 375])
 
-    corners, t, rz, box, corners_3D, pose_idx = tracklet_to_bounding_box(tracklet=tracklet,
-                                                                         cam=cam,
-                                                                         frame=frame,
-                                                                         calib_dir=calib_dir)
+    corners, t, rz, box, corners_3D, pose_idx, orientation_3D = tracklet_to_bounding_box(tracklet=tracklet,
+                                                                                         cam=cam,
+                                                                                         frame=frame,
+                                                                                         calib_dir=calib_dir)
 
     area = (box['x1'], box['y1'], box['x2'], box['y2'])
     original_area = (0, 0, image_resolution[0], image_resolution[1])
@@ -126,10 +131,10 @@ def get_x_y_data_for(tracklet, frame, cam, calib_dir, current_dir, with_image=Fa
     }
 
 
-# @timeit
+@timeit
 def main():
     drives = [
-        # 'drive_0009_sync',
+        'drive_0009_sync',
         'drive_0015_sync',
         'drive_0023_sync',
         'drive_0032_sync',
@@ -146,7 +151,7 @@ def main():
         # get number of images for this dataset
         frames = len(glob.glob(image_dir + '/*.png'))
         # start = 0
-        # end = 20
+        # end = 40
         start = 0
         end = frames
         # end = round(frames / 50)
@@ -181,7 +186,6 @@ def main():
                                           with_image=False,
                                           grayscale=True)
 
-
                 # visualization of sample
                 # buf, im = sample_to_image(sample, cam, calib_dir, current_dir)
                 # im.save('images/extraction/' + drive + '_{:d}_src_frame_{:d}.png'.format(j, frame))
@@ -196,7 +200,7 @@ def main():
         file_name = file_name + '.data'
         file = open(file_name, 'wb')
         pickle.dump(data, file)
-        print('data saved to file: ' + file_name)
+        print('data saved to file: {}, extracted {} samples.'.format(file_name, len(data)))
         file.close()
 
 
@@ -249,6 +253,13 @@ def extract_one_tracklet():
 if __name__ == '__main__':
     # extract_one_tracklet()
     main()
+    print("extraction done")
+    # print(tracklet_to_bounding_box.get_time())
+    # print(get_pointcloud.get_time())
+    # print(pointcloud_to_image.get_time())
+    # print(is_tracklet_seen.get_time())
+    # print(get_x_y_data_for.get_time())
+
     # print(load_tracklets.cache_info())
     # print(load_calibration_rigid.cache_info())
     # print(load_calibration.cache_info())

utils.py
@@ -6,13 +6,14 @@ import diskcache
 from devkit.python.load_calibration import load_calibration, load_calibration_cam_to_cam, load_calibration_rigid
 from functools import lru_cache
 from devkit.python.project import project
-from devkit.python.utils import loadFromFile, transform_to_range, load_image
+from devkit.python.utils import loadFromFile, transform_to_range, load_image, Timeit
 from devkit.python.wrapToPi import wrapToPi
 import numpy as np
 from devkit.python.projectToImage import projectToImage
 from math import cos, sin
 import matplotlib.pyplot as plt
 from PIL import Image
+from devkit.python.utils import timeit
 
 
 class Cache(diskcache.Cache):
@@ -37,9 +38,9 @@ atexit.register(lambda: cache_velo.close())
 # @lru_cache(maxsize=64)
 def get_corners(w, h, l):
     corners = {
-        'x': [l / 2,   l / 2, - l / 2, - l / 2, l / 2,   l / 2, - l / 2, - l / 2],
-        'y': [w / 2, - w / 2, - w / 2,   w / 2, w / 2, - w / 2, - w / 2,   w / 2],
-        'z': [0,           0,       0,       0,     h,       h,       h,       h]
+        'x': [l / 2, l / 2, - l / 2, - l / 2, l / 2, l / 2, - l / 2, - l / 2],
+        'y': [w / 2, - w / 2, - w / 2, w / 2, w / 2, - w / 2, - w / 2, w / 2],
+        'z': [0, 0, 0, 0, h, h, h, h]
     }
     return corners
 
@@ -59,7 +60,7 @@ def get_corners_and_orientation(corners, rz, l, t, veloToCam, cam):
     return corners_3D_cam, orientation_3D_cam
 
 
-# @lru_cache(maxsize=32)
+@lru_cache(maxsize=32)
 def get_P_velo_to_img(calib_dir, cam):
     # load calibration
     calib = load_calibration_cam_to_cam(calib_dir + '/calib_cam_to_cam.txt')
@@ -72,6 +73,8 @@ def get_P_velo_to_img(calib_dir, cam):
 
 
 # @cache_bb.memoize
+# @lru_cache(maxsize=32)
+@Timeit
 def tracklet_to_bounding_box(tracklet, cam, frame, calib_dir):
     veloToCam, K = load_calibration(dir=calib_dir)
     corners = get_corners(w=tracklet['w'], h=tracklet['h'], l=tracklet['l'])
@@ -89,7 +92,7 @@ def tracklet_to_bounding_box(tracklet, cam, frame, calib_dir):
            'y1': min(corners_2D[1, :]),
            'y2': max(corners_2D[1, :])}
 
-    return corners, t, rz, box, corners_3D, pose_idx
+    return corners, t, rz, box, corners_3D, pose_idx, orientation_3D
 
 
 def rz_to_R(rz):
@@ -99,6 +102,7 @@ def rz_to_R(rz):
     return R
 
 
+@Timeit
 def is_tracklet_seen(tracklet, frame, calib_dir, cam):
     veloToCam, K = load_calibration(calib_dir)
     image_resolution = np.array([1242, 375])
@@ -132,8 +136,9 @@ def is_tracklet_seen(tracklet, frame, calib_dir, cam):
     return True
 
 
-# @timeit
 # @cache_velo.memoize
+@lru_cache(maxsize=32)
+@Timeit
 def get_pointcloud(base_dir, frame, calib_dir, cam, area=None):
     P_velo_to_img = get_P_velo_to_img(calib_dir=calib_dir, cam=cam)
     # load velodyne points
@@ -161,14 +166,15 @@ def get_pointcloud(base_dir, frame, calib_dir, cam, area=None):
     return velo, velo_img
 
 
-# @timeit
+# @lru_cache(maxsize=32)
+@Timeit
 def pointcloud_to_image(velo, velo_img, img=None, grayscale=False):
     fig, ax = pointcloud_to_figure(velo, velo_img, img, grayscale)
     buf, im = figure_to_image(fig)
     return buf, im
 
 
-# @timeit
+@Timeit
 def pointcloud_to_figure(velo, velo_img, img=None, grayscale=False):
     image_resolution = np.array([1242, 375])
     fig = plt.figure()
@@ -184,7 +190,7 @@ def pointcloud_to_figure(velo, velo_img, img=None, grayscale=False):
     else:
         cols = matplotlib.cm.jet(np.arange(256))  # jet is colormap, represented by lookup table
         # because I want the most distant value to have more cold color (lower value)
-        col_indices = np.round(transform_to_range(1/80, 1/5, 0, 255, 1 / velo[:, 0])).astype(int)
+        col_indices = np.round(transform_to_range(1 / 80, 1 / 5, 0, 255, 1 / velo[:, 0])).astype(int)
         plt.scatter(x=velo_img[:, 0], y=velo_img[:, 1], c=cols[col_indices, 0:3], marker='o', s=1)
 
     dpi = fig.dpi
@@ -199,7 +205,7 @@ def pointcloud_to_figure(velo, velo_img, img=None, grayscale=False):
     return fig, ax
 
 
-# @timeit
+@Timeit
 def figure_to_image(fig):
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
@@ -218,10 +224,10 @@ def sample_to_image(sample, cam, calib_dir, current_dir):
     frame = metadata['frame']
     tracklet = metadata['tracklet']
 
-    corners, t, rz, box, corners_3D, pose_idx = tracklet_to_bounding_box(tracklet=tracklet,
-                                                                         cam=cam,
-                                                                         frame=frame,
-                                                                         calib_dir=calib_dir)
+    corners, t, rz, box, corners_3D, pose_idx, orientation_3D = tracklet_to_bounding_box(tracklet=tracklet,
+                                                                                         cam=cam,
+                                                                                         frame=frame,
+                                                                                         calib_dir=calib_dir)
 
     kitti_img = load_image('{:s}/image_{:02d}/data/{:010d}.png'.format(current_dir, cam, frame))
     velo = metadata['velo']