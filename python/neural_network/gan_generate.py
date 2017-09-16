810bff8e4212a09431e1f2614827aaf3a46bdb08
racinmat <racinmat@fel.cvut.cz>
2017-09-17 00:52:05 +0200

    Fixed gan generating, now uses current, slim implementation.

--- python/neural_network/gan_generate.py
+++ python/neural_network/gan_generate.py
@@ -2,6 +2,7 @@ import math
 
 from devkit.python.utils import transform_to_range
 from python.network_utils import *
+from python.neural_network.GanNetworkSlim import GanNetworkSlim
 
 
 def main():
@@ -13,42 +14,32 @@ def main():
     sess.run(tf.global_variables_initializer())
     load(sess, checkpoint_dir)
 
-    graph = tf.get_default_graph()
-    z = graph.get_tensor_by_name('z:0')
-    y = graph.get_tensor_by_name('y:0')
+    batch_size = 36
+    z_dim = 100
 
-    # samples
-    samples_num = 1
-    for idx in range(samples_num):
-        batch_size = int(z.shape[0])
-        z_dim = int(z.shape[1])
-        samples_in_batch = batch_size
-        z_sample = sample_z(samples_in_batch, z_dim)
-        # feature_vector = [0, 1, 30/100, 1, 1]
-        feature_vector = [0, 1, 2.8, 30/100, 1, 1]
-        # feature_vector = [-0.11912173, 1.00015249, 2.87348039, 0.43911632, 1., 1.]
-        # feature_vector = [0.5, 0.1, 30/100, 0.5, 1]
-        # feature_vector = [1, 0.5, 0.1, 30/100, 0.5, 1]
-        features = np.tile(feature_vector, [batch_size, 1])
+    l1_ratio = 100
+    gf_dim = 64  # (optional) Dimension of gen filters in first conv layer.
+    df_dim = 64  # (optional) Dimension of discrim filters in first conv layer.
+    gfc_dim = 1024  # (optional) Dimension of gen units for for fully connected layer.
+    dfc_dim = 1024  # (optional) Dimension of discrim units for fully connected layer.
+    c_dim = 3  # (optional) Dimension of image color. For grayscale input, set to 1, for colors, set to 3.
+    learning_rate = 0.0002  # Learning rate of for adam
+    beta1 = 0.5  # Momentum term of adam
 
-        # features = []
-        # for i in range(batch_size):
-        #     if idx is 1:
-        #         feature_vector = [1, transform_to_range(0, 36, 0, 1, i), 1, 30 / 100, 1, 1]
-        #     else:
-        #         feature_vector = [1, 1, transform_to_range(0, 36, 0, 1, i), 30 / 100, 1, 1]
-        #     features.append(feature_vector)
+    checkpoint_dir = os.path.join('tests', 'checkpoint')
 
-        image_frame_dim = int(math.ceil(batch_size ** .5))
+    network = GanNetworkSlim(checkpoint_dir)
+    image_size = (32, 32)
+    y_dim = 6
+    network.build_model(image_size, y_dim, batch_size, c_dim, z_dim, gfc_dim, gf_dim, l1_ratio, learning_rate, beta1, df_dim, dfc_dim)
+    network.load()
 
-        sampler = graph.get_tensor_by_name('generator/generator:0')    # this is last layer of generator layer
-        samples = sess.run(sampler, feed_dict={z: z_sample, y: features})
+    feature_vector = [0, 1, 2.8, 30 / 100, 1, 1]
+    features = np.tile(feature_vector, [batch_size, 1])
 
-        if not os.path.exists(os.path.dirname(samples_dir)):
-            os.makedirs(os.path.dirname(samples_dir))
+    network.generate(features, samples_dir, suffix)
 
-        save_images(samples, [image_frame_dim, image_frame_dim], '{}/test_{}_{:d}.png'.format(samples_dir, suffix, idx))
-        print("images saved to dir {}".format(samples_dir))
+    print("images saved to dir {}".format(samples_dir))
 
 
 if __name__ == '__main__':
